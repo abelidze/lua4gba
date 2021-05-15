9c46db113411073ac04dbaaaaf8070cb56f1f16d
Quicksilver <avataraang399@gmail.com>
2021-05-15 23:29:51 +1000

    Prettify demo

--- examples/demo.lua
+++ examples/demo.lua
@@ -1,12 +1,13 @@
-print("-= Demo script for Lua4gba =-\n")
+print("-= Demo script for lua4gba =-\n")
 print("*** Math test")
-print("sqrt(101)=" .. math.sqrt(101))
-print("2^100=" .. 2^100 .. "\n")
+print("sqrt(101) = " .. math.sqrt(101))
+print("2^100 = " .. 2 ^ 100 .. "\n")
 print("*** Function assignement test")
-test = function(a,b) return a*2+b*2 end
-print("test(5,7)=" .. test(5,7) .. "\n")
-print("type(test)=" .. type(test) .. "\n")
+test = function(a, b) return a * 2 + b * 2 end
+print("test(5, 7) = " .. test(5, 7))
+print("type(test) = " .. type(test) .. "\n")
 print("*** Table test")
-print("table test")
-t = { "ab","cd",5,8,13 }
-table.foreachi(t,print)
+t = { "ab", "cd", 5, 8, 13 }
+print("t = " .. tostring(t))
+print("type(t) = " .. type(t))
+table.foreachi(t, print)
